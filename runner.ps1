401197332e2bab4b67e339cf7ef2fcdcf0fab64f
nguyenminhcong2@hoaphat.com.vn <nguyenminhcong2@hoaphat.com.vn>
2025-08-20 13:35:35 +0700

    *

---
+++ runner.ps1
@@ -0,0 +1,20 @@
+﻿param(
+    [string]$FileName
+)
+
+# Tải script về máy
+$rawUrl = "https://raw.githubusercontent.com/ngm-cong/HPDQ/main/$FileName.ps1"
+$outputPath = "C:\temp\$FileName.ps1"
+
+try {
+	New-Item -Path "C:\temp" -ItemType Directory -Force
+	
+    # Tải file về máy tính
+    Invoke-WebRequest -Uri $rawUrl -OutFile $outputPath
+
+    # Chạy script đã tải về
+    & $outputPath
+
+} catch {
+    Write-Host "Đã xảy ra lỗi: $($_.Exception.Message)"
+}
